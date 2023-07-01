6319abfe4166d6905d07540de89aa625e34faa79
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-07-01 23:46:09 +0530

    Update app.js
    add API Key

--- app.js
+++ app.js
@@ -50,7 +50,7 @@
 // getVideos("cats");
 // getVideoDetails("QZHVhDjpEkE");
 const videoCardContainer = document.querySelector('.video-container');
-let api_key = "AIzaSyA23bUjUYTSyOhERLMhjvQ6ZNqh-kbL0OU";
+let api_key = "AIzaSyA7f1iE0vI2FSrjxE32J3uPSiSdjttQXyQ";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 fetch(video_http + new URLSearchParams({
